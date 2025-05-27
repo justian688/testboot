bd735f7e4760e159727d7e92d53bc0db589bf86b
justian688 <118340675+justian688@users.noreply.github.com>
2025-05-28 00:33:53 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask, request, abort
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    PushMessageRequest,   #匯入
+    BroadcastRequest,   #匯入
+    MulticastRequest,   #匯入
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+import os
+app = Flask(__name__)
+
+configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKEN'))
+line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+# 訊息事件
+@line_handler.add(MessageEvent, message=TextMessageContent)
+def message_text(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        
+        # Reply message
+
+        line_bot_api.reply_message(                         #訊息串接api
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text ='reply message0')]
+            )
+        )
+
+        result = line_bot_api.reply_message_with_http_info(     #傳訊息後回覆內容
+             ReplyMessageRequest(
+                 reply_token=event.reply_token,
+                 messages=[TextMessage(text = "reply message with http info")]
+             )
+         )
+
+         #Push message          #指定傳送對象給誰
+         #line_bot_api.push_message_with_http_info(
+           #  PushMessageRequest(
+            #     to=event.source.user_id,
+             #    messages=[TextMessage(text='PUSH!')]
+             #)
+         #)
+#
+        # Broadcast message     #簡單傳送沒差異
+        # line_bot_api.broadcast_with_http_info(
+        #     BroadcastRequest(
+        #         messages=[TextMessage(text='BROADCAST!')]
+        #     )
+        # )
+
+        
+
+
+
+if __name__ == "__main__":
+    app.run()
